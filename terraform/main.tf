d063a80cc9730206e86a4c10b11d0bdf0869c71c
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-14 23:55:23 +0900

    Import gmail route53 record

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,4 +6,27 @@ terraform {
       version = "3.6.0"
     }
   }
+  backend "s3" {
+    bucket = "tp-tfstate"
+    key    = "aws-provisioning/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_route53_record" "takkyuuplayer_com-mx-gmail" {
+  name = "takkyuuplayer.com"
+  records = [
+    "1 ASPMX.L.GOOGLE.COM",
+    "10 ALT3.ASPMX.L.GOOGLE.COM",
+    "10 ALT4.ASPMX.L.GOOGLE.COM",
+    "5 ALT1.ASPMX.L.GOOGLE.COM",
+    "5 ALT2.ASPMX.L.GOOGLE.COM",
+  ]
+  ttl     = 3600
+  type    = "MX"
+  zone_id = "Z1TMUF6CTI3C2U"
 }
