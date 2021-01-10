460dcb04b3437df3085ccd03925773a4ec475116
takkyuuplayer <takkyuuplayer@gmail.com>
2021-01-11 00:03:37 +0900

    ecs tutorial

--- cdk-exercise/MyEcsConstruct/lib/my_ecs_construct-stack.ts
+++ cdk-exercise/MyEcsConstruct/lib/my_ecs_construct-stack.ts
@@ -1,3 +1,7 @@
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+
 import * as cdk from '@aws-cdk/core';
 
 export class MyEcsConstructStack extends cdk.Stack {
@@ -5,5 +9,23 @@ export class MyEcsConstructStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 256, // Default is 256
+      desiredCount: 1, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 512, // Default is 512
+      publicLoadBalancer: true // Default is false
+    });
+
   }
 }
