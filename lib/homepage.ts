44ae456cf808aa36bc2f7f2e2067d4e3d3967a92
takkyuuplayer <takkyuuplayer@gmail.com>
2023-10-29 00:22:23 +0900

    Remove cloudfront

--- lib/homepage.ts
+++ lib/homepage.ts
@@ -1,5 +1,4 @@
-import { Stack, StackProps, aws_cloudfront, aws_s3 } from "aws-cdk-lib";
-import { OriginProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import { Stack, StackProps, aws_s3 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class HomepageStack extends Stack {
@@ -8,24 +7,11 @@ export class HomepageStack extends Stack {
 
     const homepage = new aws_s3.Bucket(this, "Bucket", {
       bucketName: "takkyuuplayer.com",
-      accessControl: aws_s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      objectOwnership: aws_s3.ObjectOwnership.BUCKET_OWNER_PREFERRED,
       versioned: true,
       websiteErrorDocument: "error.html",
       websiteIndexDocument: "index.html",
     });
-    new aws_cloudfront.CloudFrontWebDistribution(this, "Homepage", {
-      originConfigs: [
-        {
-          customOriginSource: {
-            originProtocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
-            domainName: homepage.bucketWebsiteDomainName,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-      comment: homepage.bucketName,
-    });
-
     const redirect = new aws_s3.Bucket(this, "WwwBucket", {
       bucketName: "www.takkyuuplayer.com",
       versioned: true,
@@ -33,18 +19,6 @@ export class HomepageStack extends Stack {
         hostName: "takkyuuplayer.com",
         protocol: aws_s3.RedirectProtocol.HTTPS,
       },
-    });
-    new aws_cloudfront.CloudFrontWebDistribution(this, "WwwHomepage", {
-      originConfigs: [
-        {
-          customOriginSource: {
-            originProtocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
-            domainName: redirect.bucketWebsiteDomainName,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-      comment: redirect.bucketName,
     });
   }
 }
